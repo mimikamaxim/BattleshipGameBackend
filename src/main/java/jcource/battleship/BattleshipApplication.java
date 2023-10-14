0b8404e0a1e9099e2dd15c9c461e10c0227864b2
Mimika <115660264+mimikamaxim@users.noreply.github.com>
2023-10-14 22:14:44 +0500

    create packages hierarchy
    (cherry picked from commit a54fec45b16a97654720c713678a8e566f8808d3)

--- src/main/java/jcource/battleship/BattleshipApplication.java
+++ src/main/java/jcource/battleship/BattleshipApplication.java
@@ -1,4 +1,4 @@
-package jcource.battleship.battleship;
+package jcource.battleship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/jcource/battleship/BattleshipApplicationTests.java
+++ src/test/java/jcource/battleship/BattleshipApplicationTests.java
@@ -1,4 +1,4 @@
-package jcource.battleship.battleship;
+package jcource.battleship;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
